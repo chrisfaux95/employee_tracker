8fb25f70da4f2aeb98e607658d97c3514f793428
chrisfaux95 <chrisfaux95@gmail.com>
2020-08-26 14:17:59 -0400

    edited schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,17 +3,17 @@ CREATE DATABASE employee_tracker_db;
 
 USE employee_tracker_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
@@ -23,5 +23,5 @@ CREATE TABLE employees (
     role_id INT,
     manager_id INT NULL,
     FOREIGN KEY (manager_id) REFERENCES employees(id),
-    FOREIGN KEY (role_id) REFERENCES role(id)
+    FOREIGN KEY (role_id) REFERENCES roles(id)
 );
